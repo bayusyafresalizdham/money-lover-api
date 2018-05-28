c239f12c340911695e72e1effca4121d8551df12
Bayu Syafresal Izdham <bayusyafresalizdham@gmail.com>
2018-05-29 05:26:29 +0700

    new

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require 'vendor/autoload.php';
 
 
 function auth($email,$pass){
-    $conn = new mysqli("localhost","root","","db_ml");
+    $conn = new mysqli("localhost","root","","db_moneylover");
     if($conn -> connect_error){
         die("connection failed".$conn->connect_error);
     }
@@ -32,7 +32,7 @@ function auth($email,$pass){
 
 }
 function wallet_user($token){
-    $conn = new mysqli("localhost","root","","db_ml");
+    $conn = new mysqli("localhost","root","","db_moneylover");
     if($conn -> connect_error){
         die("connection failed".$conn->connect_error);
     }
@@ -57,7 +57,7 @@ function wallet_user($token){
 }
 
 function wallet_nominal($id){
-    $conn = new mysqli("localhost","root","","db_ml");
+    $conn = new mysqli("localhost","root","","db_moneylover");
     if($conn -> connect_error){
         die("connection failed".$conn->connect_error);
     }
@@ -77,7 +77,7 @@ function wallet_nominal($id){
 
 }
 function cektoken($email){
-    $conn = new mysqli("localhost","root","","db_ml");
+    $conn = new mysqli("localhost","root","","db_moneylover");
     if($conn -> connect_error){
         die("connection failed".$conn->connect_error);
     }
@@ -112,18 +112,18 @@ $app->get('/category/{type}', function (Request $request, Response $response, ar
                 'data' => $data);
             echo json_encode($msg,JSON_PRETTY_PRINT);
         }else{
-            $conn = new mysqli("localhost","root","","db_ml");
+            $conn = new mysqli("localhost","root","","db_moneylover");
             if($conn -> connect_error){
                 die("connection failed".$conn->connect_error);
             }
             $query = "";
             if(isset($args['type'])){
                 if($args['type'] == "expense"){
-                    $query = "select * from category where type = 0";
+                    $query = "select * from category where category_type = 0";
                 }else if($args['type'] == "income"){
-                    $query = "select * from category where type = 1";
+                    $query = "select * from category where category_type = 1";
                 }else if($args['type'] == "other"){
-                    $query = "select * from category where type = 2";
+                    $query = "select * from category where category_type = 2";
                 }
             }
             $resul = $conn->query($query);
@@ -155,6 +155,58 @@ $app->post('/login', function (Request $request, Response $response, array $args
     echo json_encode($msg,JSON_PRETTY_PRINT);
 });
 
+
+$app->post('/register', function (Request $request, Response $response, array $args) {
+
+    $conn = new mysqli("localhost","root","","db_moneylover");
+    if($conn -> connect_error){
+        die("connection failed".$conn->connect_error);
+    }
+    //(`id_transaction`, `nominal`, `note`, `with_name`, `id_wallet_user`, `date`, `id_category`)
+    $query = "insert into `user` values (?,?,?,?)";
+    $stmt = $conn->prepare($query);
+    if($stmt){
+
+        $id = 0;
+        $email = $request->getParsedBody()["email"];
+        $pass = $request->getParsedBody()["password"];
+        if($email != "" || $pass != "" ){
+          $stmt->bind_param("isss",$id,$email,md5($pass),md5($email));
+          $stmt->execute();
+          //INSERT INTO `wallet_user`(`id_wallet_user`, `id_wallet_type`, `id_user`) VALUES ([value-1],[value-2],[value-3])
+          $iduser = "select * from user";
+          $resul = $conn->query($iduser);
+          $iduser1 = 0;
+          while($row = $resul->fetch_assoc()){
+              $iduser1 = $row['id_user'];
+          }
+          $idwallet = 0;
+          $query1 = "insert into `wallet_user` values (?,?,?,?)";
+          $stmt1 = $conn->prepare($query1);
+          $nama = "";
+          $type = 1;
+          $stmt1->bind_param("iiis",$idwallet,$type,$iduser1,$nama);
+          $stmt1->execute();
+          $type = 2;
+          $stmt1->bind_param("iiis",$idwallet,$type,$iduser1,$nama);
+          $stmt1->execute();
+
+          $msg = array('status' => 1,
+              'message'=>'registrasi berhasil.');
+          echo json_encode($msg,JSON_PRETTY_PRINT);
+        }else{
+          $msg = array('status' => 0,
+              'message'=>'registrasi gagal, form tidak boleh kosong.');
+          echo json_encode($msg,JSON_PRETTY_PRINT);
+        }
+    }else{
+
+        $msg = array('status' => 0,
+            'message'=>'registrasi gagal.');
+        echo json_encode($msg,JSON_PRETTY_PRINT);
+    }
+});
+
 $app->get('/category', function (Request $request, Response $response, array $args) {
     if(empty($request->getHeader("token"))){
         $data = array();
@@ -170,7 +222,7 @@ $app->get('/category', function (Request $request, Response $response, array $ar
                 'data' => $data);
             echo json_encode($msg,JSON_PRETTY_PRINT);
         }else{
-            $conn = new mysqli("localhost","root","","db_ml");
+            $conn = new mysqli("localhost","root","","db_moneylover");
             if($conn -> connect_error){
                 die("connection failed".$conn->connect_error);
             }
@@ -189,6 +241,10 @@ $app->get('/category', function (Request $request, Response $response, array $ar
 });
 
 $app->get('/wallet', function (Request $request, Response $response, array $args) {
+  $conn = new mysqli("localhost","root","","db_moneylover");
+  if($conn -> connect_error){
+      die("connection failed".$conn->connect_error);
+  }
     if(empty($request->getHeader("token"))){
         $data = array();
         $msg = array('status' => 0,
@@ -204,10 +260,49 @@ $app->get('/wallet', function (Request $request, Response $response, array $args
             echo json_encode($msg,JSON_PRETTY_PRINT);
         }else{
             $data = wallet_user($request->getHeader("token")[0]);
-
+            $iduser= cektoken($request->getHeader("token")[0]);
+            $query = "select * from wallet_user where id_wallet_type = 3 and id_user =".$iduser;
+            $resul = $conn->query($query);
+            $c = count($data);
+            while($row = $resul->fetch_assoc()){
+                $data[$c]['id_wallet'] = $row['id_wallet_user'];
+                $data[$c]['name_wallet'] = $row['name_from_user'];
+                $data[$c]['nominal_wallet'] = 0;
+                $c++;
+            }
             $msg = array('status' => 1,
                 'message'=>'Category berhasil di ambil',
                 'data' => $data);
+            echo json_encode($msg,JSON_PRETTY_PRINT);
+        }
+    }
+});
+$app->post('/wallet', function (Request $request, Response $response, array $args) {
+  $conn = new mysqli("localhost","root","","db_moneylover");
+  if($conn -> connect_error){
+      die("connection failed".$conn->connect_error);
+  }
+    if(empty($request->getHeader("token"))){
+        $msg = array('status' => 0,
+            'message'=>'token tidak ada');
+        echo json_encode($msg,JSON_PRETTY_PRINT);
+    }else{
+        if(cektoken($request->getHeader("token")[0]) == ""){
+            $data = array();
+            $msg = array('status' => 0,
+                'message'=>'token anda salah');
+            echo json_encode($msg,JSON_PRETTY_PRINT);
+        }else{
+            $iduser = cektoken($request->getHeader("token")[0]);
+            $idwallet = 0;
+            $query1 = "insert into `wallet_user` values (?,?,?,?)";
+            $stmt1 = $conn->prepare($query1);
+            $nama = $request->getParsedBody()["walletname"];;
+            $type = 3;
+            $stmt1->bind_param("iiis",$idwallet,$type,$iduser,$nama);
+            $stmt1->execute();
+            $msg = array('status' => 1,
+                'message'=>'wallet berhasil ditambah');
             echo json_encode($msg,JSON_PRETTY_PRINT);
         }
     }
@@ -226,7 +321,7 @@ $app->post('/transaction', function (Request $request, Response $response, array
             echo json_encode($msg,JSON_PRETTY_PRINT);
         }else{
 
-            $conn = new mysqli("localhost","root","","db_ml");
+            $conn = new mysqli("localhost","root","","db_moneylover");
             if($conn -> connect_error){
                 die("connection failed".$conn->connect_error);
             }
@@ -275,12 +370,51 @@ $app->get('/transaction/{id_wallet}', function (Request $request, Response $resp
                 'data' => $data);
             echo json_encode($msg,JSON_PRETTY_PRINT);
         }else{
-            $conn = new mysqli("localhost","root","","db_ml");
+            $data = array();
+            $conn = new mysqli("localhost","root","","db_moneylover");
             if($conn -> connect_error){
                 die("connection failed".$conn->connect_error);
             }
             //SELECT `id_transaction`, `nominal`, `note`, `with_name`, `id_wallet_user`, `date`, `id_category` FROM `transaction` WHERE 1
             $query = "select * from `transaction` t,`category` c  where t.id_category = c.id_category and id_wallet_user = ".$args["id_wallet"];
+            $resul = $conn->query($query);
+            while($row = $resul->fetch_assoc()){
+                $data[] = $row;
+            }
+
+            $msg = array('status' => 1,
+                'message'=>'Transaction berhasil di ambil',
+                'data' => $data);
+            echo json_encode($msg,JSON_PRETTY_PRINT);
+        }
+    }
+});
+$app->get('/transaction/{id_wallet}/{tgl}', function (Request $request, Response $response, array $args) {
+
+    if(empty($request->getHeader("token"))){
+        $data = array();
+        $msg = array('status' => 0,
+            'message'=>'token tidak ada',
+            'data' => $data);
+        echo json_encode($msg,JSON_PRETTY_PRINT);
+    }else{
+        if(cektoken($request->getHeader("token")[0]) == ""){
+            $data = array();
+            $msg = array('status' => 0,
+                'message'=>'token anda salah',
+                'data' => $data);
+            echo json_encode($msg,JSON_PRETTY_PRINT);
+        }else{
+            $data = array();
+            $conn = new mysqli("localhost","root","","db_moneylover");
+            if($conn -> connect_error){
+                die("connection failed".$conn->connect_error);
+            }
+
+            $nominal = $request->getParsedBody()["date"];
+            $tgl = $args["tgl"];
+            //SELECT `id_transaction`, `nominal`, `note`, `with_name`, `id_wallet_user`, `date`, `id_category` FROM `transaction` WHERE 1
+            $query = "select * from `transaction` t,`category` c  where t.id_category = c.id_category and id_wallet_user = '".$args["id_wallet"]."' and t.date = '".$tgl."'";;
             $resul = $conn->query($query);
             while($row = $resul->fetch_assoc()){
                 $data[] = $row;
